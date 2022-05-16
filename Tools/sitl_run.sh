72aa88613a4cdac2da85ab35d1267feab8414994
Massimiliano Mocellin <mocellinmax97@gmail.com>
2022-05-16 18:38:13 +0200

    Meccanismo attivo

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -167,7 +167,7 @@ elif [ "$program" == "gazebo" ] && [ ! -n "$no_sim" ]; then
 		fi
 		echo "Starting gzserver"
 		#while gz model --verbose --spawn-file="${modelpath}/${model}/${model_name}.sdf" --model-name=${model} -x 1.01 -y 0.98 -z 0.83 2>&1 | grep -q "An instance of Gazebo is not running."; do
-		while gz model --verbose --spawn-file="${modelpath}/${model}/${model_name}.sdf" --model-name=${model} -x 0 -y 0 -z 1 2>&1 | grep -q "An instance of Gazebo is not running."; do
+		while gz model --verbose --spawn-file="${modelpath}/${model}/${model_name}.sdf" --model-name=${model} -x 0 -y 0 -z 0.5 2>&1 | grep -q "An instance of Gazebo is not running."; do
 			echo "gzserver not ready yet, trying again!"
 			sleep 5
 		done
